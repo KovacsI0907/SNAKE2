7e775baa155f0030fc39032adec513263eaa958d
István Kovács <kovacsi0907@gmail.com>
2024-06-10 11:40:03 +0200

    3d alap program csak win64

--- graphics/src/main.cpp
+++ graphics/src/main.cpp
@@ -1,16 +1,255 @@
-#include <iostream>
-#include "module1/module1.h"
-#include "module2/module2.h"
+//=============================================================================================
+//  My first Green Triangle
+//=============================================================================================
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main() {
-    int a = 3;
-    int b = 4;
+#if defined(__APPLE__)
+#include <GLUT/GLUT.h>
+#include <OpenGL/gl3.h>
+#else
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#include <windows.h>
+#endif
+#include <GL/glew.h>		// Extension Wrangler: must be downloaded
+#include <GL/freeglut.h>	// Toolkit for window management: must be downloaded
+#endif
 
-    int sum = add(a, b);
-    int product = multiply(a, b);
+const unsigned int windowWidth = 600, windowHeight = 600;
 
-    std::cout << "Sum: " << sum << std::endl;
-    std::cout << "Product: " << product << std::endl;
+// OpenGL major and minor versions
+int majorVersion = 3, minorVersion = 3;
 
-    return 0;
+// Gets error messare after shader compilation
+void getErrorInfo(unsigned int handle) {
+	int logLen;
+	glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &logLen);
+	if (logLen > 0) {
+		char* log = new char[logLen];
+		int written;
+		glGetShaderInfoLog(handle, logLen, &written, log);
+		printf("Shader log:\n%s", log);
+		delete log;
+	}
+}
+
+// check if shader could be compiled
+void checkShader(unsigned int shader, char* message) {
+	int OK;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &OK);
+	if (!OK) {
+		printf("%s!\n", message);
+		getErrorInfo(shader);
+	}
+}
+
+// check if shader could be linked
+void checkLinking(unsigned int program) {
+	int OK;
+	glGetProgramiv(program, GL_LINK_STATUS, &OK);
+	if (!OK) {
+		printf("Failed to link shader program!\n");
+		getErrorInfo(program);
+	}
+}
+
+// vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
+const char* vertexSource = R"(
+	#version 330
+	precision highp float;		// normal floats, makes no difference on desktop computers
+
+	uniform mat4 MVP;			// uniform variable, the Model-View-Projection transformation matrix
+	layout(location = 0) in vec2 vp;	// Varying input: vp = vertex position is expected in attrib array 0
+
+	void main() {
+		gl_Position = vec4(vp.x, vp.y, 0, 1) * MVP;		// transform vp from modeling space to normalized device space
+	}
+)";
+
+// fragment shader in GLSL
+const char* fragmentSource = R"(
+	#version 330
+	precision highp float;	// normal floats, makes no difference on desktop computers
+	
+	uniform vec3 color;		// uniform variable, the color of the primitive
+	out vec4 outColor;		// computed color of the current pixel
+
+	void main() {
+		outColor = vec4(color,1);	// computed color is the color of the primitive
+	}
+)";
+
+unsigned int shaderProgram; // handle of the shader program
+unsigned int vao; // virtual world on the GPU
+
+const int nc = 20, nv = 100;
+// Initialization, create an OpenGL context
+void onInitialization() {
+	glViewport(0, 0, windowWidth, windowHeight);
+
+	glGenVertexArrays(1, &vao);	// get 1 vao id
+	glBindVertexArray(vao);		// make it active
+
+	unsigned int vbo;		// vertex buffer object
+	glGenBuffers(1, &vbo);	// Generate 1 buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// Geometry with 24 bytes (6 floats or 3 x 2 coordinates)
+	float vertices[] = { -0.8f, -0.8f,  -0.6f, 1.0f,  0.8f, -0.2f };
+	glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
+		sizeof(vertices),  // # bytes
+		vertices,	      	// address
+		GL_STATIC_DRAW);	// we do not change later
+
+	glEnableVertexAttribArray(0);  // AttribArray 0
+	glVertexAttribPointer(0,       // vbo -> AttribArray 0
+		2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
+		0, NULL); 		     // stride, offset: tightly packed
+
+	// Create vertex shader from string
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	if (!vertexShader) {
+		printf("Error in vertex shader creation\n");
+		exit(1);
+	}
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+	checkShader(vertexShader, "Vertex shader error");
+
+	// Create fragment shader from string
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	if (!fragmentShader) {
+		printf("Error in fragment shader creation\n");
+		exit(1);
+	}
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	checkShader(fragmentShader, "Fragment shader error");
+
+	// Attach shaders to a single program
+	shaderProgram = glCreateProgram();
+	if (!shaderProgram) {
+		printf("Error in shader program creation\n");
+		exit(1);
+	}
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	// Connect the fragmentColor to the frame buffer memory
+	glBindFragDataLocation(shaderProgram, 0, "fragmentColor");	// fragmentColor goes to the frame buffer memory
+
+	// program packaging
+	glLinkProgram(shaderProgram);
+	checkLinking(shaderProgram);
+	// make this program run
+	glUseProgram(shaderProgram);
+}
+
+// Window has become invalid: Redraw
+void onDisplay() {
+	glClearColor(0, 0, 0, 0);     // background color
+	glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
+
+	// Set color to (0, 1, 0) = green
+	int location = glGetUniformLocation(shaderProgram, "color");
+	glUniform3f(location, 0.0f, 1.0f, 0.0f); // 3 floats
+
+	float MVPtransf[4][4] = { 1, 0, 0, 0,    // MVP matrix, 
+							  0, 1, 0, 0,    // row-major!
+							  0, 0, 1, 0,
+							  0, 0, 0, 1 };
+
+	location = glGetUniformLocation(shaderProgram, "MVP");
+	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);
+
+	glBindVertexArray(vao);  // Draw call
+	glDrawArrays(GL_TRIANGLES, 0 /*startIdx*/, 3 /*# Elements*/);
+
+	glutSwapBuffers(); // exchange buffers for double buffering
+}
+
+// Key of ASCII code pressed
+void onKeyboard(unsigned char key, int pX, int pY) {
+	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+}
+
+// Key of ASCII code released
+void onKeyboardUp(unsigned char key, int pX, int pY) {
+}
+
+// Move mouse with key pressed
+void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
+	// Convert to normalized device space
+	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
+	float cY = 1.0f - 2.0f * pY / windowHeight;
+	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
+}
+
+// Mouse click event
+void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
+	// Convert to normalized device space
+	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
+	float cY = 1.0f - 2.0f * pY / windowHeight;
+
+	char* buttonStat;
+	switch (state) {
+	case GLUT_DOWN: buttonStat = "pressed"; break;
+	case GLUT_UP:   buttonStat = "released"; break;
+	}
+
+	switch (button) {
+	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
+	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
+	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
+	}
+}
+
+// Idle event indicating that some time elapsed: do animation here
+void onIdle() {
+	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+	glutPostRedisplay();
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Do not touch the code below this line
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+#if !defined(__APPLE__)
+	glutInitContextVersion(majorVersion, minorVersion);
+#endif
+	glutInitWindowSize(windowWidth, windowHeight);				// Application window is initially of resolution 600x600
+	glutInitWindowPosition(100, 100);							// Relative location of the application window
+#if defined(__APPLE__)
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_3_2_CORE_PROFILE);  // 8 bit R,G,B,A + double buffer + depth buffer
+#else
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+#endif
+	glutCreateWindow(argv[0]);
+
+#if !defined(__APPLE__)
+	glewExperimental = true;	// magic
+	glewInit();					// initialize glew and opengl
+#endif
+
+	// print the current settings
+	printf("GL Vendor    : %s\n", glGetString(GL_VENDOR));
+	printf("GL Renderer  : %s\n", glGetString(GL_RENDERER));
+	printf("GL Version (string)  : %s\n", glGetString(GL_VERSION));
+	glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+	glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+	printf("GL Version (integer) : %d.%d\n", majorVersion, minorVersion);
+	printf("GLSL Version : %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	onInitialization();							// Initialization is done here
+
+	glutDisplayFunc(onDisplay);                // Register event handlers
+	glutMouseFunc(onMouse);
+	glutIdleFunc(onIdle);
+	glutKeyboardFunc(onKeyboard);
+	glutKeyboardUpFunc(onKeyboardUp);
+	glutMotionFunc(onMouseMotion);
+
+	glutMainLoop();
+	return 1;
 }
