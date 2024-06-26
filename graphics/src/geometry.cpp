cf258591adb4104aec2c2d2e3d05dd8339649be9
István Kovács <kovacsi0907@gmail.com>
2024-06-26 19:51:22 +0200

    Generic geometry class

---
+++ graphics/src/geometry.cpp
@@ -0,0 +1,37 @@
+#include <geometry.h>
+#include <GL/glew.h>
+
+void Geometry::create() {
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glGenBuffers(3, vbos);
+
+	if (vertices.size() != normals.size() || vertices.size() != uvs.size())
+		throw InvalidGeometryException("Vertices, normals and uvs count is not equal");
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbos[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * normals.size(), &normals[0], GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbos[2]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * uvs.size(), &uvs[0], GL_STATIC_DRAW);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(2);
+}
+
+void Geometry::draw(GLenum mode) {
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);
+	glDrawArrays(mode, 0, vertices.size());
+}
+
+Geometry::~Geometry() {
+	glDeleteBuffers(3, vbos);
+	glDeleteVertexArrays(1, &vao);
+}
