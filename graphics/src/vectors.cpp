ac23964d1ec8c59576422bbb37823a28f3a2119d
István Kovács <kovacsi0907@gmail.com>
2024-06-27 12:31:30 +0200

    New vector functions

--- graphics/src/vectors.cpp
+++ graphics/src/vectors.cpp
@@ -26,3 +26,39 @@ vec4 operator*(const vec4& v, const mat4& m) {
         v.x * m.rows[0][3] + v.y * m.rows[1][3] + v.z * m.rows[2][3] + v.w * m.rows[3][3]
     );
 }
+
+vec2 normalize(const vec2& v) {
+    float len = v.length();
+    return { v.x / len, v.y / len };
+}
+
+vec3 normalize(const vec3& v) {
+    float len = v.length();
+    return { v.x / len, v.y / len, v.z / len };
+}
+
+vec4 normalize(const vec4& v) {
+    float len = v.length();
+    return { v.x / len, v.y / len, v.z / len, v.w / len };
+}
+
+float dot(const vec2& a, const vec2& b) {
+    return a.x * b.x + a.y * b.y;
+}
+
+float dot(const vec3& a, const vec3& b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+float dot(const vec4& a, const vec4& b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+}
+
+vec3 cross(const vec3& a, const vec3& b) {
+    return {
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    };
+}
+
