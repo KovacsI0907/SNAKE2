a672c6b179f25b05cb09e138024806617e776385
István Kovács <kovacsi0907@gmail.com>
2024-06-27 14:23:05 +0200

    Camera sample program

--- graphics/src/main.cpp
+++ graphics/src/main.cpp
@@ -9,16 +9,25 @@
 #include <shadersources.h>
 #include <triangle.h>
 #include <cylinder.h>
+#include <camera.h>
 
-using namespace std;
 
 const unsigned int windowWidth = 640, windowHeight = 480;
 
-ShaderProgram shaderProgram = ShaderProgram(VERTEX_SOURCE, FRAGMENT_SOURCE, GEOMETRY_SOURCE);
-Triangle tri = Triangle(vec3(0, 0, 0), vec3(1, 1, 0), vec3(1, 0, 0));
+ShaderProgram shaderProgram = ShaderProgram(VERTEX_SOURCE, FRAGMENT_SOURCE);
+PerspectiveCamera pCam = PerspectiveCamera(vec3(1, -2, 2), vec3(0, 0, 0), (float)windowWidth / windowHeight, M_PI / 3);
+OrthographicCamera oCam = OrthographicCamera(vec3(1, -2, 2), vec3(0, 0, 0), (float)windowWidth / windowHeight, 3, 100);
+Camera* activeCam = &pCam;
+Cylinder cyl = Cylinder(50);
+Triangle tri = Triangle(vec3(-1.5, 0, 0), vec3(1.5, 0, 0), vec3(0, 0, 1));
 
 void initialize() {
 	glViewport(0, 0, windowWidth, windowHeight);
+
+	glEnable(GL_DEPTH_TEST);
+
+	cyl.create();
+	tri.create();
 	
 	shaderProgram.compile();
 	shaderProgram.use();
@@ -26,12 +35,28 @@ void initialize() {
 
 void onDisplay() {
 	glClearColor(0.2f, 0.2f, 0.2f, 0.f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	shaderProgram.setUniform("color", vec3(1.0f, 0.0f, 1.0f));
-	shaderProgram.setUniform("MVP", mat4::identity());
+	shaderProgram.setUniform("MVP", activeCam->view() * activeCam->projection());
+
+	tri.draw();
+	shaderProgram.setUniform("color", vec3(1.0f, 1.0f, 0.0f));
+	cyl.draw();
 
 	glutSwapBuffers();
+}
+
+void onKeyPressed(unsigned char key, int x, int y) {
+	if (key == 'c') {
+		if (activeCam == &pCam) {
+			activeCam = &oCam;
+		}
+		else {
+			activeCam = &pCam;
+		}
+	}
+	glutPostRedisplay();
 }
 
 int main(int argc, char* argv[]) {
@@ -47,6 +72,7 @@ int main(int argc, char* argv[]) {
 	initialize();
 
 	glutDisplayFunc(onDisplay);
+	glutKeyboardFunc(onKeyPressed);
 
 	glutMainLoop();
 	return 1;
