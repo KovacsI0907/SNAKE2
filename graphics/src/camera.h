21e0fcfd4402429a93ffcf507f59bae4efe36858
István Kovács <kovacsi0907@gmail.com>
2024-06-27 14:21:39 +0200

    Added generic, perspective and orthographic cameras

--- graphics/src/camera.h
+++ graphics/src/camera.h
@@ -24,13 +24,28 @@ class PerspectiveCamera : public Camera {
 private:
 	float frontPlane;
 	float backPlane;
-	float fov; //vertical
 public:
+	float fov; //vertical
+
 	PerspectiveCamera(vec3 eye, vec3 lookAt, float asp, float verticalFov, vec3 preferredUp = vec3(0,0,1), float frontPlane = 0.1f, float backPlane = 100.0f) : Camera(eye, lookAt, asp, preferredUp) {
 		this->frontPlane = frontPlane;
 		this->backPlane = backPlane;
 		fov = verticalFov;
 	}
+	mat4 view();
+	mat4 projection();
+};
+
+class OrthographicCamera : public Camera {
+public:
+	float height;
+	float depth;
+
+	OrthographicCamera(vec3 eye, vec3 lookAt, float aspectRatio, float height, float depth, vec3 preferredUp = vec3(0, 0, 1)) : Camera(eye, lookAt, aspectRatio, preferredUp) {
+		this->height = height;
+		this->depth = depth;
+	}
+
 	mat4 view();
 	mat4 projection();
 };
