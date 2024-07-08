95cb8b48a233fcaa5a58b61b70fbceb4dab2e8fb
Attila0818 <zsigmondattila2003@gmail.com>
2024-07-08 18:28:10 +0200

    fix :D

--- graphics/src/main.cpp
+++ graphics/src/main.cpp
@@ -13,6 +13,7 @@
 #include <scene.h>
 #include <exception>
 #include <chesstex.h>
+#include <sph.h>
 
 
 const unsigned int windowWidth = 640, windowHeight = 480;
@@ -28,29 +29,19 @@ Camera* activeCamera = &pCam;
 Scene scene = Scene();
 Light light = { vec4(3, -3, 3, 0), vec4(1,1,1,1) };
 
-ChessTex* chessTex = new ChessTex(vec4(184.0f/255, 135.0f/255, 98.0f/255, 1), vec4(233.0f/255, 211.0f/255, 173.0f/255, 1), 50,1);
+ChessTex* chessTex = new ChessTex(vec4(184.0f/255, 135.0f/255, 98.0f/255, 1), vec4(233.0f/255, 211.0f/255, 173.0f/255, 1), 25,25);
 Material cylMat = { vec4(1, 1, 1, 1), vec4(1, 0, 0, 0), vec4(1,1,1,1), 5.0f, chessTex};
-Material triMat = { vec4(1, 1, 1, 1), vec4(1, 1, 0, 0), vec4(0,0,0,1), 25.0f };
-Material reaMat = { vec4(1, 1, 1, 1), vec4(233.0f / 255, 211.0f / 255, 173.0f / 255, 1), vec4(0,0,0,1), 25.0f };
-Cylinder* cyl = new Cylinder(50);
-Triangle* tri = new Triangle(vec3(-1.5, 0, 0), vec3(1.5, 0, 0), vec3(0, 0, 1));
-Sphere* sph = new Sphere(50);
+auto sph = std::make_unique<Sphere>(500);
 
 
 
 void initialize() {
+
 	glViewport(0, 0, windowWidth, windowHeight);
-
 	glEnable(GL_DEPTH_TEST);
-
 	scene.light = light;
+	scene.addObject(std::make_unique<Object>(std::move(sph), cylMat));
 
-	Object* c = new Object(cyl, cylMat);
-	Object* t = new Object(tri, triMat);
-
-	//scene.addObject(c);
-	//scene.addObject(t);
-	
 	try {
 		shaderProgramGouraud.compile();
 		shaderProgramPhong.compile();
